0d03ba040720e15968bcc525aa643a5bd423de6d
Angel Tsang <angeltsang@Angels-MacBook-Pro.local>
2021-06-06 13:43:56 -0700

    session 50, update navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-header',
-    templateUrl:'./header.component.html',
-    styleUrls:[]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: [],
 })
-
-export class HeaderComponent{}
+export class HeaderComponent {
+  collapsed = true;
+}
