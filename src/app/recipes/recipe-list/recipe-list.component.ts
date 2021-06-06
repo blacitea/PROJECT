fa0d04ab2522c202c7e715d6f05b18190b0a7e2c
Angel Tsang <angeltsang@Angels-MacBook-Pro.local>
2021-06-06 14:12:21 -0700

    use model in to construct recipe in recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core'
-
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 @Component({
-    selector:'app-recipe-list',
-    templateUrl:'./recipe-list.component.html',
-    styleUrls:[]
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: [],
 })
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'A recipe for testing the model',
+      'https://images-gmi-pmc.edge-generalmills.com/122984eb-b5ee-48a9-a509-29cf9a26505b.jpg'
+    ),
+  ];
+  constructor() {}
 
-export class RecipeListComponent {}
+  ngOnInit() {}
+}
