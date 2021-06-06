8d7cadde28c3b1de66affe95ff172f0e1fc03c35
Angel Tsang <angeltsang@Angels-MacBook-Pro.local>
2021-06-06 14:36:55 -0700

    add ingredient model and update html to display the ingredients in shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: [],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Banana', 3),
+    new Ingredient('Egg', 3),
+  ];
   constructor() {}
   ngOnInit() {}
 }
